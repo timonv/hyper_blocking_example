abf87a012a07faeab0702ee9d7aa403e297750b8
Timon Vonk <timonv@gmail.com>
2015-02-25 07:50:02 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![feature(old_io)]
+
+extern crate hyper;
+
+use std::sync::Mutex;
+use std::sync::mpsc::channel;
+use std::old_io::net::ip::Ipv4Addr;
+use hyper::server::{Server, Request, Response};
+
+fn main() {
+    let message = get_message();
+    println!("Never called: {}", message)
+}
+
+fn get_message() -> String {
+    let server = Server::http(Ipv4Addr(127, 0, 0, 1), 9999);
+
+    let (tx, rx) = channel();
+    let mtx = Mutex::new(tx);
+
+    let mut guard = server.listen(move |_: Request, _: Response| {
+        println!("Request received!");
+        mtx.lock().unwrap().send("Boosh!").unwrap();
+        println!("Request ended!");
+    }).unwrap();
+
+    let message = rx.recv().unwrap();
+    guard.close().unwrap();
+    println!("{}", message);
+    message.to_string()
+}
